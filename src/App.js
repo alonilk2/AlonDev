f7bc39792300b188dfa22494a6424280bf0f721a
alonilk2 <63236463+alonilk2@users.noreply.github.com>
2022-02-26 02:04:44 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "@fontsource/encode-sans-sc/700.css";
+import "@fontsource/encode-sans/700.css";
+import Zoom from 'react-reveal/Zoom';
+import HorizontalScroll from 'react-scroll-horizontal'
+
+
 
 function App() {
+  
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [pageScrollState, setPageScrollState] = useState(0);
+
+  const handleScroll = (e) => {
+      console.log(e.deltaY);
+      if(e.deltaY > 0) setPageScrollState(pageScrollState => pageScrollState-1);
+      else setPageScrollState(pageScrollState => pageScrollState+1);
+  };
+
+  useEffect(() => {
+      window.addEventListener('wheel', handleScroll, { passive: true });
+      return () => {
+          window.removeEventListener('wheel', handleScroll);
+      };
+  }, []);
+
+  useEffect(() => {
+    console.log(pageScrollState)
+  }, [pageScrollState])
+  
+  const renderPages = () => {
+    switch(pageScrollState){
+      case 0: 
+          return (
+                  <section class="content custom-animation-white">
+        <div class="content-body">
+          <h1 className= "content-title delay-1">Software Engineer</h1>
+          <h1 className="content-title-2 full-stack custom-animation-gradient delay-2">Full-Stack</h1>
+          <h1 className="content-title-2 developer custom-animation-white delay-1"> Developer</h1>
+        </div>
+      </section>
+        )
+        break;
+      case 1: 
+          return (
+                  <section class="content animation-horizontal">
+        <div class="content-body">
+          <h1 className= "content-title custom-animation-white delay-1">Software Engineer</h1>
+          <h1 className="content-title-2 full-stack custom-animation-gradient delay-2">Full-Stack</h1>
+          <h1 className="content-title-2 developer custom-animation-white delay-1"> Developer</h1>
+        </div>
+      </section>
+          )
+        break;
+      
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="bg">
+      <header class="header">
+        <h3 className="title custom-animation-gradient">Alon Barenboim</h3>
+
+        <button className="btn custom-animation-white">Projects</button>
+        <button className="btn custom-animation-white">About Me</button>
+        <button className="btn custom-animation-white">Contact</button>
       </header>
+            {/* <Zoom> */}
+      {renderPages()}
+
+      {/* </Zoom> */}
+      <footer class="footer">
+        <p>This is the footer.</p>
+      </footer>
     </div>
   );
 }
