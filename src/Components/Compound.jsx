c8c047e543ab1bc5fe779a7aca950838d959fb47
Alon Barenboim <alonilk2@gmail.com>
2022-05-06 05:06:34 +0300

    1

---
+++ src/Components/Compound.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+import ReactIcon from "../Images/React-icon.png";
+import metamask from "../Images/metamask.png";
+import Hardhat from "../Images/hardhat.png";
+import Ethers from "../Images/ethers.png";
+import Alchemy from "../Images/alchemy.png";
+import "../App.css";
+import CrossfadeCarousel from "@notbaldrick/react-crossfade-carousel";
+import Flip from "react-reveal/Flip";
+import Fade from "react-reveal/Fade";
+
+function Compound(props) {
+  return (
+    <>
+      <section className={props.className} style={{ margin: "0 2%" }}>
+        <div className="content-body-project">
+          <div
+            className="col-4"
+            style={{
+              margin: "5% 2%",
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              alignItems: "center",
+            }}
+          >
+            <Flip top when={props.state}>
+              <h1
+                className="content-title-project delay-1"
+                style={{ position: "unset", height: "unset" }}
+              >
+                Compound DApp
+              </h1>{" "}
+            </Flip>
+            <Flip top when={props.state} delay={500}>
+              <p
+                className="description"
+                style={{ position: "unset", height: "unset" }}
+              >
+                A single-page web application for Compound DeFi Protocol
+                services, featuring ETH coins supply and DAI coins borrow. The
+                app is interfacing with MetaMask wallets.
+              </p>{" "}
+            </Flip>
+            <Fade left when={props.state} delay={800}>
+              <div
+                className="row project-info"
+                style={{ position: "unset", height: "unset" }}
+              >
+                <Fade left when={props.state} delay={1000}>
+                  <div className="col tech-col">
+                    <img
+                      className="row tech-logo"
+                      src={ReactIcon}
+                      style={{
+                        width: "90%",
+                        position: "unset",
+                        height: "unset",
+                      }}
+                      alt="react"
+                    />
+                    <p className="row stack-title">React.js</p>
+                  </div>{" "}
+                </Fade>
+                <Fade left when={props.state} delay={1200}>
+                  <div className="col tech-col">
+                    <img
+                      className="tech-logo"
+                      src={Ethers}
+                      alt="Ethers.js"
+                      style={{
+                        transform: "scale(1.5)",
+                        marginBottom: "20px",
+                        position: "unset",
+                        height: "unset",
+                      }}
+                    />
+                    <p className="stack-title">Ethers.js</p>
+                  </div>{" "}
+                </Fade>
+                <Fade left when={props.state} delay={1400}>
+                  <div className="col tech-col">
+                    <img
+                      className="tech-logo"
+                      src={metamask}
+                      alt="metamask"
+                      style={{ position: "unset", height: "unset" }}
+                    />
+                    <p className="stack-title">MetaMask</p>
+                  </div>{" "}
+                </Fade>
+                <Fade left when={props.state} delay={1600}>
+                  <div className="col tech-col">
+                    <img
+                      className="tech-logo"
+                      src={Hardhat}
+                      alt="HardhatSQL"
+                      style={{ position: "unset", height: "unset" }}
+                    />
+                    <p className="stack-title">Hardhat</p>
+                  </div>{" "}
+                </Fade>
+                <Fade left when={props.state} delay={1800}>
+                  <div className="col tech-col">
+                    <img
+                      className="tech-logo"
+                      src={Alchemy}
+                      alt="Alchemy"
+                      style={{ position: "unset", height: "unset" }}
+                    />
+                    <p className="stack-title">Alchemy</p>
+                  </div>{" "}
+                </Fade>
+              </div>
+            </Fade>
+
+            <a
+              className="content-title-visit delay-1 "
+              style={{ textAlign: "center", textDecoration: 'unset', zIndex: 3}}
+              href={'https://alonilk2.github.io/compound-dapp/'}
+            >
+              Enter App
+            </a>
+          </div>
+
+          <div class="image-cropper cf4a">
+            <CrossfadeCarousel
+              interval={3000}
+              transition={2000}
+              images={["https://alonilk2.github.io/map1/compound.jpg"]}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Compound;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,168 @@
+import React, { Suspense, useEffect, useRef, useState, useMemo } from "react";
+import Fade from "react-reveal/Fade";
+import { Canvas, extend, useThree, useFrame } from "@react-three/fiber";
+import BlackHole from "./BlackHole";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import * as THREE from "three";
+import useWindowSize from "../Hooks/useWindowSize";
+
+extend({ EffectComposer, RenderPass, UnrealBloomPass });
+
+function Bloom({ children }) {
+  const { gl, camera, size } = useThree();
+  const [scene, setScene] = useState();
+  const composer = useRef();
+  const aspect = useMemo(
+    () => new THREE.Vector2(size.width, size.height),
+    [size]
+  );
+  useEffect(
+    () => void scene && composer.current.setSize(size.width, size.height),
+    [size]
+  );
+  useFrame(() => scene && composer.current.render(), 1);
+  return (
+    <>
+      <scene ref={setScene}>{children}</scene>
+      <effectComposer ref={composer} args={[gl]}>
+        <renderPass attachArray="passes" scene={scene} camera={camera} />
+        <unrealBloomPass attachArray="passes" args={[aspect, 3, 0.8, 0]} />
+      </effectComposer>
+    </>
+  );
+}
+function Home(props) {
+  const size = useWindowSize();
+  const [showTitle, setShowTitle] = useState(false);
+  const [showTitle1, setShowTitle1] = useState(false);
+  const [showTitleGradient, setShowTitleGradient] = useState(false);
+  const [RemoveCol, setRemoveCol] = useState(false);
+
+  const initialRender = useRef(true);
+
+  useEffect(() => {
+    if (initialRender.current === true) {
+      initialRender.current = false;
+
+      setTimeout(() => {
+        setShowTitle(true);
+      }, 1200);
+      setTimeout(() => {
+        setShowTitle(false);
+      }, 3000);
+      setTimeout(() => {
+        setShowTitle1(true);
+      }, 3500);
+      setTimeout(() => {
+        setShowTitleGradient(true);
+      }, 5500);
+      setTimeout(() => {
+        setRemoveCol(true);
+      }, 7000);
+    }
+  });
+
+  return (
+    <>
+      <section
+        className={props.className}
+        style={{ marginTop: 0}}
+      >
+        {size.width > 768 && (
+          <Fade opposite left when={props.state} duration={2000} delay={1000}>
+            <iframe
+              style={{
+                height: "60%",
+                width: "20%",
+                marginLeft: "-7%",
+              }}
+              src="https://embed.lottiefiles.com/animation/14325"
+            ></iframe>
+          </Fade>
+        )}
+        {!RemoveCol && (
+          <div
+            style={{
+              width: "100%",
+              // height: "90px",
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            {showTitleGradient ? (
+              <h3
+                className="content-title alon-title title1 custom-animation-gradient1"
+                style={{ textAlign: "center", width: "100%" }}
+              >
+                <span className="fade-animate">I'm</span> Alon
+              </h3>
+            ) : (
+              <>
+                <Fade opposite when={showTitle} duration={2000}>
+                  <h3
+                    className="content-title content-title-project title1 "
+                    style={{ textAlign: "center" }}
+                  >
+                    Hi
+                  </h3>
+                </Fade>
+                <Fade when={showTitle1} duration={2000}>
+                  <h3
+                    className="content-title content-title-project title1 "
+                    style={{ textAlign: "center" }}
+                  >
+                    I'm Alon
+                  </h3>
+                </Fade>
+              </>
+            )}
+          </div>
+        )}
+
+        <Fade bottom delay={6000} duration={2000}>
+          <div className="col" style={{ marginTop: "8.5%" }}>
+            <h1 className="content-title linear-wipe">FULL-STACK ENGINEER</h1>
+          </div>
+        </Fade>
+        {size.width > 768 ? (
+          <Canvas
+            camera={{ position: [0, 1, 8] }}
+            style={{
+              zIndex: 0,
+              position: "absolute",
+              width: "100%",
+            }}
+          >
+            {/* <Bloom> */} <ambientLight intensity={1} />
+            <Suspense fallback={null}>
+              <BlackHole />
+            </Suspense>
+            {/* </Bloom> */}
+          </Canvas>
+        ) : (
+          <Canvas
+            camera={{ position: [0, 2, 11] }}
+            style={{
+              zIndex: 0,
+              width: "100%",
+              position: "absolute",
+              top: "5%",
+              minHeight: "500px",
+            }}
+          >
+            {/* <Bloom> */} <ambientLight intensity={1} />
+            <Suspense fallback={null}>
+              <BlackHole />
+            </Suspense>
+            {/* </Bloom> */}
+          </Canvas>
+        )}
+      </section>
+    </>
+  );
+}
+
+export default Home;
